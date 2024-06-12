dae7eebb8bddfd805427ddec6af01e605aa18d4d
Ricardo Garcia <ricardogarcia@MacBook-Air-de-Ricardo.local>
2024-06-12 09:01:11 -0600

    Added new Navigation link

--- CursoiOS/MenuView.swift
+++ CursoiOS/MenuView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct MenuView: View {
     var body: some View {
         NavigationStack{
-            List {
-                NavigationLink(destination:IMCView()) {
-                    Text("IMC Calculator")
-                }
-                NavigationLink(destination:SuperHeroSearcher()) {
-                    Text("Superhero finder")
-                }
-                NavigationLink(destination:FavPlaces()) {
-                    Text("Fav Places")
-                }
-                NavigationLink(destination:ViewPager()) {
-                    Text("View Pager")
+            VStack {
+                TitleText(text: "Mis apps")
+                List {
+                    NavigationLink(destination:IMCView()) {
+                        Text("IMC Calculator")
+                    }
+                    NavigationLink(destination:SuperHeroSearcher()) {
+                        Text("Superhero finder")
+                    }
+                    NavigationLink(destination:FavPlaces()) {
+                        Text("Fav Places")
+                    }
+                    NavigationLink(destination:ViewPager()) {
+                        Text("View Pager Planes")
+                    }
                 }
             }
         }
